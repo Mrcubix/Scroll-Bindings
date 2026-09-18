253248cd764633a3e70a9ef6afb266f460772b2e
Gess1t <tintamare91@gmail.com>
2026-09-18 19:50:51 +0200

    Add option to sync cursor on Scroll Binding use Disable Legacy Binding & associated settings tool

--- ScrollBinding-0.6.x/BulletproofScrollBinding.cs
+++ ScrollBinding-0.6.x/BulletproofScrollBinding.cs
@@ -53,6 +53,19 @@ public class NewBulletproofScrollBinding : BulletproofScrollBinding
         set => _scrollAmount = Math.Clamp(value, 0, 2400);
     }
 
+    [BooleanProperty("Sync Cursor On Press", ""), 
+     DefaultPropertyValue(false),
+     ToolTip("Scroll Binding:\n\n" +
+             "Artist Mode &amp; Windows Ink Mode uses a different cursor.\n" +
+             "This may result in the binding scrolling in the completely wrong place.\n" +
+             "Note: Only some Linux Destop Environments will require this.\n\n" +
+             "Default: Disabled")]
+    public bool SyncCursorOnPress
+    {
+        get => _syncCursorOnPress;
+        set => _syncCursorOnPress = value;
+    }
+
     public static new IEnumerable<string> ValidOptions => BulletproofScrollBinding.ValidOptions;
 
     #endregion
